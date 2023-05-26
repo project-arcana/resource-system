573ad7c6e50871c425a30394b31ebf48efec9642
Philip Trettner <Philip.Trettner@rwth-aachen.de>
2023-05-26 15:17:08 +0200

    working on new system

--- src/resource-system/base/api.cc
+++ src/resource-system/base/api.cc
@@ -1,51 +1,95 @@
 #include "api.hh"
 
+#include <clean-core/experimental/ringbuffer.hh>
+#include <clean-core/hash.sha1.hh>
+#include <clean-core/indices_of.hh>
+#include <clean-core/intrinsics.hh>
 #include <clean-core/map.hh>
 #include <clean-core/vector.hh>
 
+#include <rich-log/log.hh>
+
+#include <resource-system/detail/log.hh>
+
+#include <shared_mutex>
+
+namespace res::base
+{
 namespace
 {
+// TODO: flat?
+struct res_desc
+{
+    comp_hash comp;
+    cc::vector<res_hash> args;
+
+    // cache for resources
+    bool is_enqueued = false;
+    int content_gen = -1;
+    content_hash content_name;
+    content_data content_data; // TODO: refcounting
+};
+
+struct content_desc
+{
+    cc::array<std::byte> data_backing_buffer;
+    content_data data;
+};
+
+// TODO: flat?
+struct invoc_desc
+{
+    // TODO: do we really need to store this?
+    // comp_hash comp;
+    // cc::vector<content_hash> args;
+
+    content_hash content;
+
+    // cached shortcut
+    // is not persistet
+    content_data data; // TODO: refcounting
+};
+
 struct res_hash_hasher
 {
-    size_t operator()(res::base::hash const& h) const { return reinterpret_cast<size_t const&>(h); }
+    size_t operator()(res::base::hash const& h) const { return h.w0; }
 };
+
+template <class HashT>
+HashT finalize_as(cc::sha1_builder& b)
+{
+    auto sha1_value = b.finalize();
+    static_assert(sizeof(sha1_value) >= sizeof(HashT));
+    HashT hash;
+    std::memcpy(&hash, &sha1_value, sizeof(hash));
+    return hash;
 }
+} // namespace
+} // namespace res::base
 
 struct res::base::ResourceSystem::impl
 {
     // TODO
 
-    struct comp_desc
-    {
-        // comp metadata
-        // arg size and types
-        // return type
-        // executor stuff
-    };
-
-    // TODO: flat?
-    struct res_desc
-    {
-        comp_hash comp;
-        cc::vector<res_hash> args;
-    };
-
-    struct content_desc
-    {
-        cc::array<std::byte> data;
-    };
-
-    // TODO: flat?
-    struct invoc_desc
-    {
-        comp_hash comp;
-        cc::vector<content_hash> args;
-    };
-
-    cc::map<comp_hash, comp_desc, res_hash_hasher> comp_desc_by_hash;
+    // for now, we need comp/res maps completely in memory
+    // so we know how to compute every resource
+    cc::map<comp_hash, computation_desc, res_hash_hasher> comp_desc_by_hash;
     cc::map<res_hash, res_desc, res_hash_hasher> res_desc_by_hash;
+
+    // these two are the "data caches"
     cc::map<content_hash, content_desc, res_hash_hasher> content_desc_by_hash;
     cc::map<invoc_hash, invoc_desc, res_hash_hasher> invoc_desc_by_hash;
+
+    // db sync
+    std::shared_mutex comp_desc_by_hash_mutex;
+    std::shared_mutex res_desc_by_hash_mutex;
+    std::shared_mutex content_desc_by_hash_mutex;
+    std::shared_mutex invoc_desc_by_hash_mutex;
+
+    // queue
+    // TODO: mpmc with grow?
+    cc::ringbuffer<res_hash> queue_resource_to_compute;
+    std::mutex queue_resource_to_compute_mutex;
 };
 
 res::base::ResourceSystem::ResourceSystem() { m = cc::make_unique<impl>(); }
@@ -54,12 +98,310 @@ res::base::ResourceSystem::~ResourceSystem() = default;
 
 res::base::comp_hash res::base::ResourceSystem::define_computation(computation_desc desc)
 {
-    // TODO
-    return comp_hash();
+    // make hash
+    cc::sha1_builder sha1;
+    sha1.add(cc::as_byte_span(desc.name));
+    sha1.add(cc::as_byte_span(desc.algo_hash));
+    // TODO: args?
+    auto const hash = finalize_as<comp_hash>(sha1);
+
+    auto& db = m->comp_desc_by_hash;
+    auto& mutex = m->comp_desc_by_hash_mutex;
+
+    // read: check if comp already known
+    mutex.lock_shared();
+    auto const has_value = db.contains_key(hash);
+    // TODO: verify if stored desc is the same
+    mutex.unlock_shared();
+
+    // write: add to map
+    if (!has_value)
+    {
+        mutex.lock();
+        db[hash] = cc::move(desc);
+        mutex.unlock();
+    }
+
+    return hash;
 }
 
 res::base::res_hash res::base::ResourceSystem::define_resource(comp_hash const& computation, cc::span<res_hash const> args)
 {
-    // TODO
-    return res_hash();
+    // make hash
+    cc::sha1_builder sha1;
+    sha1.add(cc::as_byte_span(computation));
+    for (auto const& h : args)
+        sha1.add(cc::as_byte_span(h));
+    auto const hash = finalize_as<res_hash>(sha1);
+
+    auto& db = m->res_desc_by_hash;
+    auto& mutex = m->res_desc_by_hash_mutex;
+
+    // read: check if comp already known
+    mutex.lock_shared();
+    auto const has_value = db.contains_key(hash);
+    mutex.unlock_shared();
+
+    // write: add to map
+    if (!has_value)
+    {
+        res_desc desc;
+        desc.comp = computation;
+        desc.args.push_back_range(args);
+
+        mutex.lock();
+        db[hash] = cc::move(desc);
+        mutex.unlock();
+    }
+
+    return hash;
+}
+
+cc::optional<res::base::content_data> res::base::ResourceSystem::try_get_resource_content(res_hash res, bool start_computation_if_not_found)
+{
+    cc::optional<content_data> result;
+    auto need_compute = false;
+    int const target_generation = generation;
+
+    auto& db = m->res_desc_by_hash;
+    auto& mutex = m->res_desc_by_hash_mutex;
+
+    // 1. read-only res db lookup
+    //    this is the fast path
+    mutex.lock_shared();
+    auto p_desc = db.get_ptr(res);
+    if (p_desc)
+    {
+        // see if cached version found
+        if (p_desc->content_gen == target_generation)
+        {
+            // we guarantee that this is the stored data
+            result = p_desc->content_data;
+        }
+        else
+        {
+            // cached content is either :
+            // - outdated
+            // - computed but not cached
+            // - not computed
+            need_compute = true;
+        }
+    }
+    mutex.unlock_shared();
+
+    if (!p_desc)
+    {
+        LOG_ERROR("no resource known with id %s", cc::as_byte_span(res));
+        return result;
+    }
+
+    // 2. if no cached data found, trigger computation
+    if (need_compute && start_computation_if_not_found)
+    {
+        // set to enqueued in res db
+        mutex.lock();
+        p_desc = db.get_ptr(res);
+        CC_ASSERT(p_desc && "overzealous GC?");
+        auto need_enqueue = false;
+        if (!p_desc->is_enqueued)
+        {
+            need_enqueue = true;
+            p_desc->is_enqueued = true;
+        }
+        mutex.unlock();
+
+        // actually enqueue
+        if (need_enqueue)
+        {
+            auto& queue = m->queue_resource_to_compute;
+            auto& queue_mutex = m->queue_resource_to_compute_mutex;
+
+            queue_mutex.lock();
+            queue.push_back(res);
+            queue_mutex.unlock();
+        }
+    }
+
+    return result;
+}
+
+void res::base::ResourceSystem::invalidate_impure_resources()
+{
+    // simple for now
+    cc::intrin_atomic_add(&generation, 1);
+}
+
+void res::base::ResourceSystem::process_all()
+{
+    auto& queue = m->queue_resource_to_compute;
+    auto& queue_mutex = m->queue_resource_to_compute_mutex;
+
+    auto& db = m->res_desc_by_hash;
+    auto& db_mutex = m->res_desc_by_hash_mutex;
+
+    cc::vector<res_hash> args;
+    cc::vector<cc::optional<content_data>> args_content;
+    cc::vector<content_hash> args_content_hashes;
+
+    // not locked because this is fine to be approximative
+    while (!queue.empty())
+    {
+        res_hash res;
+        auto has_res = true;
+
+        // get job
+        queue_mutex.lock();
+        if (!queue.empty())
+            res = queue.pop_front();
+        else
+            has_res = false;
+        queue_mutex.unlock();
+
+        if (!has_res)
+            break; // spurior non-empty
+
+        // get job info job
+        args.clear();
+        comp_hash comp;
+        auto gen = generation;
+
+        // TODO: is it better to look up dependencies directly here?
+        // TODO: do we need to check if it's already up to date here?
+        db_mutex.lock_shared();
+        auto p_desc = db.get_ptr(res);
+        CC_ASSERT(p_desc && "overzealous GC?");
+        args.push_back_range(p_desc->args);
+        comp = p_desc->comp;
+        db_mutex.unlock_shared();
+
+        // query arg content
+        // NOTE: we query all args, even if some are missing
+        //       this reduces latency and improves parallelism
+        auto has_all_args = true;
+        args_content.resize(args.size());
+        for (auto i : cc::indices_of(args))
+        {
+            // TODO: only content_hash required
+            //       this makes a difference when loading content lazily from file
+            args_content[i] = this->try_get_resource_content(args[i], true);
+            if (!args_content[i].has_value())
+                has_all_args = false;
+        }
+
+        // not all args available? requeue
+        if (!has_all_args)
+        {
+            queue_mutex.lock();
+            queue.push_back(res);
+            queue_mutex.unlock();
+            continue;
+        }
+
+        // we have all args here
+        args_content_hashes.resize(args.size());
+        for (auto i : cc::indices_of(args))
+            args_content_hashes[i] = args_content[i].value().hash;
+
+        // TODO
+        // - make invoc
+        // - check if invoc cached
+        // - otherwise invoce comp fun
+        //   and store content
+
+        // done
+        // TODO: store in res db
+    }
+}
+
+cc::optional<res::base::content_data> res::base::ResourceSystem::query_content(content_hash hash)
+{
+    cc::optional<content_data> result;
+
+    auto& db = m->content_desc_by_hash;
+    auto& mutex = m->content_desc_by_hash_mutex;
+
+    mutex.lock_shared();
+    if (auto p_content = db.get_ptr(hash))
+        result = p_content->data;
+    mutex.unlock_shared();
+
+    return result;
+}
+
+res::base::content_hash res::base::ResourceSystem::store_content(cc::span<const std::byte> data)
+{
+    cc::sha1_builder sha1;
+    sha1.add(data);
+    auto const hash = finalize_as<content_hash>(sha1);
+
+    auto& db = m->content_desc_by_hash;
+    auto& mutex = m->content_desc_by_hash_mutex;
+
+    content_desc desc;
+    desc.data_backing_buffer = cc::array<std::byte>(data); // copy
+    desc.data.hash = hash;
+    desc.data.state = content_state::serialized_data;
+    desc.data.data.serialized = desc.data_backing_buffer;
+
+    mutex.lock();
+// TODO: must probably be deleted because we can get this easily in multithreaded context
+#ifndef CC_RELEASE
+    if (db.contains_key(hash))
+        LOG_WARN("duplicated content store for %s", cc::as_byte_span(hash));
+#endif
+    db[hash] = cc::move(desc);
+    mutex.unlock();
+
+    return hash;
+}
+
+res::base::invoc_hash res::base::ResourceSystem::define_invocation(comp_hash const& computation, cc::span<const content_hash> args)
+{
+    cc::sha1_builder sha1;
+    sha1.add(cc::as_byte_span(computation));
+    for (auto const& h : args)
+        sha1.add(cc::as_byte_span(h));
+    return finalize_as<invoc_hash>(sha1);
+}
+
+cc::optional<res::base::invoc_result> res::base::ResourceSystem::query_invocation(invoc_hash hash, bool try_query_data)
+{
+    cc::optional<invoc_result> opt_result;
+
+    auto& db = m->invoc_desc_by_hash;
+    auto& mutex = m->invoc_desc_by_hash_mutex;
+
+    // look up cached invocation
+    mutex.lock_shared();
+    if (auto p_desc = db.get_ptr(hash))
+    {
+        invoc_result result;
+        result.content = p_desc->content;
+        result.data = p_desc->data;
+        opt_result = result;
+    }
+    mutex.unlock_shared();
+
+    // check if content should be queried
+    if (try_query_data &&         //
+        opt_result.has_value() && //
+        opt_result.value().data.state != content_state::invalid)
+    {
+        auto h_content = opt_result.value().content;
+        auto d_content = this->query_content(h_content);
+
+        // write back to db
+        if (d_content.has_value())
+        {
+            opt_result.value().data = d_content.value();
+
+            mutex.lock();
+            auto& res = db[hash];
+            res.content = h_content;
+            res.data = d_content.value();
+            mutex.unlock();
+        }
+    }
+
+    return opt_result;
 }
--- src/resource-system/base/api.hh
+++ src/resource-system/base/api.hh
@@ -3,8 +3,10 @@
 #include <cstddef>
 #include <cstdint>
 
-#include <clean-core/polymorphic.hh>
+#include <clean-core/optional.hh>
 #include <clean-core/span.hh>
+#include <clean-core/string.hh>
+#include <clean-core/unique_function.hh>
 #include <clean-core/unique_ptr.hh>
 
 // [hash based resource system]
@@ -54,13 +56,18 @@
 namespace res::base
 {
 
-// TODO: other size? 128 bit? 256 bit?
+// see https://en.wikipedia.org/wiki/Birthday_problem#Probability_table
+// - 128 bit hash
+// - 10^10 objects in the database
+// - 10^-18 probability of at least one collision
 struct alignas(16) hash
 {
     uint64_t w0;
     uint64_t w1;
+
+    constexpr bool operator==(hash const& h) const { return w0 == h.w0 && w1 == h.w1; }
+    constexpr bool operator!=(hash const& h) const { return w0 != h.w0 || w1 != h.w1; }
 };
-// static_assert(sizeof(hash) == 20);
 
 struct comp_hash : hash
 {
@@ -75,20 +82,73 @@ struct invoc_hash : hash
 {
 };
 
-// TODO: lifetimes / refcounting
-class KeyValueStorageBase : public cc::polymorphic
+enum class content_state
 {
-public:
-    virtual cc::span<std::byte const> get(hash const& h) const = 0;
-    virtual void set(hash const& h, cc::span<std::byte const> data) = 0;
+    // not valid content
+    invalid = 0,
+    // valid but the content is an error (e.g. exception during execution)
+    error,
+    // valid and the data is serialized
+    serialized_data,
+    // valid and the data is void* (unserializable)
+    opaque_data,
+};
+
+struct opaque_data
+{
+    // TODO: invoc hash as content_hash?
+    void* data_ptr;
+    cc::function_ptr<void(void*)> deleter;
+};
+
+struct content_data
+{
+    content_hash hash;
+    content_state state = content_state::invalid;
+
+    // if this is a true, then the data is not necessarily the "most current"
+    // it's still accessible but will change in the future
+    bool is_outdated = false;
+
+    union data_t
+    {
+        cc::span<std::byte const> serialized;
+        opaque_data opaque;
+
+        data_t() {}
+    } data;
 };
 
 struct computation_desc
 {
+    cc::string name;
     hash algo_hash;
+
+    cc::unique_function<void()> compute_resource;
 
     // TODO: where can this be computed?
     // TODO: is this immediate or multipart?
+    // comp metadata
+    // arg size and types
+    // return type
+    // executor stuff
+    // "impure resources" (should be stored in res_desc probably)
+
+    // nonserializable resources:
+    // - content_hash = invocation_hash
+    // - no persisting
+    // - otherwise easy to support
+
+    // NOTE: we want the following features
+    // - computation can be done in other threads / custom queues
+    // - computation can be async + multistep
+    // - computation can return a resource that should be evaluated
+};
+
+struct invoc_result
+{
+    content_hash content;
+    content_data data;
 };
 
 /// a resource system manages access / computation / lifetimes of resources
@@ -97,6 +157,11 @@ struct computation_desc
 ///
 /// NOTE: this version implements the base API
 ///       in particular, there are very few safety rails on purpose
+///
+/// MULTITHREADING: all public methods are threadsafe
+///
+/// TODO: refcounting for comp+res
+///       can be done by returning pointers to an atomic int
 class ResourceSystem
 {
     ResourceSystem();
@@ -109,14 +174,56 @@ class ResourceSystem
 
     // core operations
 public:
+    // NOTE: defining the same computation twice returns the same hash
+    //       there are some debug checks to prevent easy errors
+    // TODO: split into mandatory (aka hash-defining part)
+    //       and lazy stuff (aka populating the desc)
+    // TODO: refcounting?
     comp_hash define_computation(computation_desc desc);
+
+    // TODO: flags for impure resources here?
+    // TODO: refcounting?
     res_hash define_resource(comp_hash const& computation, cc::span<res_hash const> args);
 
+    // TODO: error states?
+    // TODO: non-serialized type
+    // TODO: access to outdated data
+    cc::optional<content_data> try_get_resource_content(res_hash res, bool start_computation_if_not_found = true);
+
+    // invalidates all impure resources such as file timestamps or tweakable data
+    // this is an extremely cheap O(1) operation
+    // it will cause gradual recompution of all dependent resources
+    // though in practice, most will hit the content caches anyways
+    void invalidate_impure_resources();
+
+    // processing API
+public:
+    // NOTE: THIS IS DEBUG API
+    void process_all();
+
+    // internal core operations
+    // TODO: does it make sense to expose these?
+private:
+    // TODO: error states?
+    cc::optional<content_data> query_content(content_hash hash);
+
+    // TODO: error states?
+    // TODO: version that can move data?
+    content_hash store_content(cc::span<std::byte const> data);
+
+    // NOTE: this is really fast and does not need DB access
+    invoc_hash define_invocation(comp_hash const& computation, cc::span<content_hash const> args);
+
+    // TODO: error states?
+    cc::optional<invoc_result> query_invocation(invoc_hash hash, bool try_query_data = true);
 
 private:
     // all complex implementation is pimpl'd to keep the header clean
     struct impl;
     cc::unique_ptr<impl> m;
+
+    // generation counter used for O(1) invalidation of locally cached content
+    int volatile generation = 1000;
 };
 
-}
+} // namespace res::base
